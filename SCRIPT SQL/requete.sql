15656f4ec6c2648401346f9a8e3e0340048f95ae
Andyx <127594368+andyxrakoto@users.noreply.github.com>
2023-08-24 00:37:32 +0300

    Update requete.sql

--- SCRIPT SQL/requete.sql
+++ SCRIPT SQL/requete.sql
@@ -2,48 +2,36 @@ CREATE DATABASE entreprise;
 
 \c entreprise
 
-CREATE TABLE IF NOT EXISTS Employees (
-    id_employee SERIAL PRIMARY KEY,
-    first_name VARCHAR(150),    
-    last_name VARCHAR(150),
-    position VARCHAR(50),
-    hire_date DATE,
-    salary float,
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES Departments(id_department)
+-- Creating the "Departments" table
+CREATE TABLE Departments (
+    ID INT PRIMARY KEY,
+    DepartmentName VARCHAR(255),
+    Description VARCHAR(255)
 );
 
-
-CREATE TABLE IF NOT EXISTS Departments (
-    id_department SERIAL PRIMARY KEY,
-    department_name VARCHAR(100)
+-- Creating the "Employees" table
+CREATE TABLE Employees (
+    ID INT PRIMARY KEY,
+    LastName VARCHAR(255),
+    FirstName VARCHAR(255),
+    DateOfBirth DATE,
+    Address VARCHAR(255),
+    PhoneNumber VARCHAR(15),
+    EmailAddress VARCHAR(255),
+    HireDate DATE,
+    JobTitle VARCHAR(255),
+    Salary FLOAT,
+    DepartmentID INT,
+    FOREIGN KEY (DepartmentID) REFERENCES Departments(ID)
 );
 
-CREATE TABLE IF NOT EXISTS Leaves (
-    id_leave SERIAL PRIMARY KEY,
-    employee_id INT,
-    start_date DATE,
-    end_date DATE,
-    reason VARCHAR(100),
-    FOREIGN KEY (employee_id) REFERENCES Employees(id_employee)
-);
-
-
-CREATE TABLE IF NOT EXISTS Salaries (
-    id_salary SERIAL PRIMARY KEY,
-    employee_id INT,
-    amount float,
-    payment_date DATE,
-    FOREIGN KEY (employee_id) REFERENCES Employees(id_employee)
-);
-
-
-CREATE TABLE IF NOT EXISTS Tasks (
-    id_task SERIAL PRIMARY KEY,
-    employee_id INT,
-    description VARCHAR(200),
-    assignment_date DATE,
-    due_date DATE,
-    status VARCHAR(20),
-    FOREIGN KEY (employee_id) REFERENCES Employees(id_employee)
+-- Creating the "Leaves" table
+CREATE TABLE Leaves (
+    ID INT PRIMARY KEY,
+    EmployeeID INT,
+    StartDate DATE,
+    EndDate DATE,
+    LeaveReason VARCHAR(255),
+    LeaveStatus VARCHAR(255),
+    FOREIGN KEY (EmployeeID) REFERENCES Employees(ID)
 );
